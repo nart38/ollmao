bb474597bc9adada8dd38034e9ac9b43b2d3d4c5
nart38 <sezer_1966_sezgin@hotmail.com>
2024-02-09 10:10:08 +0300

    Add EncodeJson, ExtractAnswer, ChatRequest methods.

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	"github.com/nart38/ollama-bubble/lib"
 )
 
 func main() {
-	// TODO Accept model name as command line argument.
-	api.ConstructData("Can you describe Go programing language to me?")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ollama-bubble model-name")
+		os.Exit(1)
+	}
+	model := os.Args[1]
+
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("User Prompt: ")
+		prompt, _ := stdin.ReadString('\n')
+		api.ChatRequest(prompt, model)
+	}
 }
